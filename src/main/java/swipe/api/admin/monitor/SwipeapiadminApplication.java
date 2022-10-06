0674f31117c9a012d772ab3b4119b62a0677b8cd
Isaiah Omame <omameazy@yahoo.com>
2022-10-06 16:23:12 -0700

    Update on addition of context path

--- src/main/java/swipe/api/admin/monitor/SwipeapiadminApplication.java
+++ src/main/java/swipe/api/admin/monitor/SwipeapiadminApplication.java
@@ -3,13 +3,18 @@ package swipe.api.admin.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableAdminServer
 @SpringBootApplication
+@PropertySource(value = "file:/opt/appdocs/springbootadmin/config/application.properties", ignoreResourceNotFound = true)
+
+//@PropertySource(value = "file:C:\\SwipeTech\\Projects\\APPLICATION_PROPERTIES\\springbootadmin\\application.properties", ignoreResourceNotFound = true)
+
 public class SwipeapiadminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwipeapiadminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwipeapiadminApplication.class, args);
+    }
 
 }
